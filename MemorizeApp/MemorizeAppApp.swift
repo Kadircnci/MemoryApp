ce20f30b7d2b9253277bf0e3911cf6cdda1b0aec
kadir <kadircncii@gmail.com>
2024-06-09 22:18:50 +0300

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Kadir Çanakcı on 9.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
